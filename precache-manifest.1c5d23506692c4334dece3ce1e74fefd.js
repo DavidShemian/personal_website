287a6ec323d3377ce741776c3a3d439e71ad9d8d
DavidShemian18 <DavidShemian18@gmail.com>
2019-05-23 16:32:06 +0300

    Updates

---
+++ precache-manifest.1c5d23506692c4334dece3ce1e74fefd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b9b343d2d0829d738cedfc5878001f",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "de8cdd3f7320a5726210",
+    "url": "/personal-website/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cf3119b1632bd6e85ddf",
+    "url": "/personal-website/static/js/2.762bced1.chunk.js"
+  },
+  {
+    "revision": "de8cdd3f7320a5726210",
+    "url": "/personal-website/static/js/main.732bfd60.chunk.js"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "f8193096d09cee0c43d4a19b49712975",
+    "url": "/personal-website/static/media/resume.f8193096.pdf"
+  }
+]);
