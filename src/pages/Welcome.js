f287990ca6cc53374eb18e3824f24a148778cf8b
DavidShemian18 <DavidShemian18@gmail.com>
2019-05-30 14:49:50 +0300

    muted video

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -8,7 +8,7 @@ const Welcome = () => {
     return <Container id={'Welcome'}>
         <BasicText text={`Hey, I'm David Shemian`} fontStyle={'italic'} color={MAIN_WHITE} fontSize={'1.5em'} isTitle />
         <BasicText text={`Fullstack developer`} color={MAIN_WHITE} isTitle />
-        <Video autoPlay loop>
+        <Video autoPlay loop muted>
             <Source src={video} type="video/mp4" />
         </Video>
     </Container>
