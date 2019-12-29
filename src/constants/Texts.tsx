120d175bbbff90cc7f1f221d622bd3a71b8af585
David Shemian <davidshemian18@gmail.com>
2019-12-29 21:41:37 +0200

    edited some text

--- src/constants/Texts.tsx
+++ src/constants/Texts.tsx
@@ -2,9 +2,9 @@ export const SMALL_RUMBLE_DESCRIPTION: string = `Rumble is an app intended to fi
 I worked at rumble as a full stack developer using Node js and React. 
 My responsibilities varied from creating and maintaining an online tool that makes our database accessible to the non-developers in the company,
  adding new features to the app, creating automatic reports from the data of the company, and much more.
-  Since Rumble is still a small company, I had to be very independent in it. 
-  When I started working at Rumble, as a second year student, I had to learn React and create an external tool in just 3 months of part time job,  all by myself.
-  I feel that this improved my ability to learn new things in an efficient, quick way.`;
+  As I was working in a small startup, independence was a necessary skill. 
+  When I started working at Rumble, as a second year student, I managed to learn React and create an external tool in just 3 months.
+  This experience has improved my ability to learn new things in an efficient, quick manner.`;
 
 export const XPERITI_DESCRIPTION: string = `Xperiti is an AI-powered enterprise software designed to give 
   investment management firms a strategic advantage in research.
@@ -12,10 +12,10 @@ export const XPERITI_DESCRIPTION: string = `Xperiti is an AI-powered enterprise 
   log system, event sourcing system, the authentication APIs and much more.
   In Xperiti we work using Node JS and Typescript for the backend, and Angular for the front end.`;
 
-export const IDC_DESCRIPTION: string = `While studying at IDC I got the chance to learn many things about myself and about the world.
-I learned that I love studying that there are amazing people out there to learn so much from.
+export const IDC_DESCRIPTION: string = `While studying at IDC I got the chance to learn many things about myself and about the programming world.
+I learned that I love studying and learning new things, and I was especially inspired by the talented professionals who taught me. 
 The main thing that I learned is that no matter what obstacle I'm facing, with the right amount of dedication and hard work I can overcome it. 
-IDC offered me a challenging yet highly rewarding environment, thank to it great programs and amazing professors.`;
+Studying towards my BS degree has offered me a challenging yet highly rewarding environment, thanks to great extracurricular programs and amazing professors.`;
 
 export const JAVASCRIPT_DESCRIPTION: string = `My bread and butter. 
 I have been working with Javascript (and Typescript) for the last year and a half, with great enjoyment.
